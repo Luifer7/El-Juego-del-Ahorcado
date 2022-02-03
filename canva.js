cede69052577bfad85265b9499df75ca2e3cb7af
Luis Fernando Cordero <98068290+DobleuStrange@users.noreply.github.com>
2022-02-03 00:10:29 -0500

    Update canva.js

--- canva.js
+++ canva.js
@@ -4,68 +4,155 @@
 
     let cuadro = document.getElementById("lienzo");
     let contexto = cuadro.getContext('2d');
-
-
+    
+    contexto.fillStyle = "white"
+    
+    
+    
+    
     function vidaSiete() {
         
-        contexto.moveTo(180,270)
-        contexto.lineTo(140, 350)
-        contexto.lineWidth = 8
+        contexto.fillStyle = '#34130B', 'border: 1 solid blak;';
+        contexto.fillRect(20,20, 14, 330)
+        contexto.lineCap = 'round';
+        contexto.fillRect(20,20, 45, 20)
+        contexto.linecap = "round";
        
+            contexto.beginPath();
+            contexto.strokeStyle = '#34130B';
+            contexto.lineWidth = 10;
+            contexto.moveTo(25,100);
+            contexto.lineTo(60,35);
+            contexto.stroke();
+            contexto.lineCap = 'round';
+
+            
+
+        contexto.beginPath();
+        contexto.strokeStyle = '#34130B';
+        contexto.lineWidth = 8;
+        contexto.moveTo(0,350)
+        contexto.lineTo(27, 320)
+        contexto.lineTo(53, 350)
+        contexto.fill(); 
         contexto.stroke();
-    }
+        contexto.lineCap = 'round';
+            
+        
+    }   
+
 
     function vidaSeis() {
-        contexto.moveTo(180,270)
-        contexto.lineTo(220, 350)
-        contexto.stroke();
+    
+    contexto.fillRect(20,20,160,20)
+    contexto.fillRect(180, 20, 8, 60 )
+    contexto.lineCap = 'round';
+            
     }
+
 
     function vidaCinco() {
-        contexto.moveTo(180,141)
-        contexto.lineTo(180, 270)
-        contexto.stroke();
-    
+
+      contexto.beginPath();
+      contexto.strokeStyle = 'white';
+      contexto.lineWidth = 8;
+      contexto.arc(180,110,30,0,2*Math.PI)      
+      contexto.stroke();
+      contexto.lineCap = 'round';
+
+                
     }
+         
 
     function vidaCuatro() {
-        contexto.moveTo(180,141)
-        contexto.lineTo(280, 165)
+
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 8;
+        contexto.moveTo(119,163)
+        contexto.lineTo(180, 141)     
         contexto.stroke();
-    
+        contexto.lineCap = 'round';
+            
+           
     }
+
 
     function vidaTres() {
-        contexto.moveTo(75,163)
-        contexto.lineTo(180, 141)
-        contexto.stroke();
+
         contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 8;
+        contexto.moveTo(180,141)
+        contexto.lineTo(245, 165)    
+        contexto.stroke();
+        contexto.lineCap = 'round';
         
     }
+
 
     function vidaDos() {
-        contexto.arc(180,110,30,0,2*Math.PI)
+
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 8;
+        contexto.moveTo(180,141)
+        contexto.lineTo(180, 270)    
         contexto.stroke();
-
-      
-        
-    }
-
-    function vidaUno() {
-        
-   
-    contexto.fillRect(180, 20, 8, 60 )
-        
-    }
-
-    function vidaCero() {
-        
-    contexto.fillRect(0,20,180,10)
+        contexto.lineCap = 'round';
     
     }
 
 
 
+    function vidaUno() {
+
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 8;
+        contexto.moveTo(180,270)
+        contexto.lineTo(200, 330)   
+        contexto.stroke();
+        contexto.lineCap = 'round';
+        
+    }
+
+  
+
+    function vidaCero() {
+
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 8;
+        contexto.moveTo(180,270)
+        contexto.lineTo(160, 330)  
+        contexto.stroke();
+
+        
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 3;
+        contexto.arc(171,99,5,0,2*Math.PI)
+        contexto.stroke();
+
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.lineWidth = 3;
+        contexto.arc(190,99,5,0,2*Math.PI)
+        contexto.stroke();
+
+        contexto.beginPath();
+        contexto.strokeStyle = 'white';
+        contexto.arc(180,127,13,0,1*Math.PI,true)
+        contexto.stroke();
+        contexto.lineWidth = 3;
+        contexto.lineCap = 'round';
+        
+
+
+        
+       
+    }
 
 
 
